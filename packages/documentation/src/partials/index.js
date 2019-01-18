1c0e6d2062a819617a3aab6550261ec3e07c7943
Piotr F <hello@piotrf.pl>
2019-01-18 13:09:30 +0100

    Cleanup.

--- packages/documentation/src/partials/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Layout } from './layout/Layout';
-export { default as LayoutBd } from './layout/LayoutBd';
-export { default as LayoutFt } from './layout/LayoutFt';
-export { default as LayoutHd } from './layout/LayoutHd';
-export { default as Topbar } from './topbar/Topbar';
