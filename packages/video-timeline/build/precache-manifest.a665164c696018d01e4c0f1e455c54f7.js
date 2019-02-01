0c3d7a5174918b67ccb8fa98faee3702ab5f1f02
Laurian Gridinoc <laurian@gridinoc.name>
2019-02-01 14:49:26 -0800

    storing build files because bower

---
+++ packages/video-timeline/build/precache-manifest.a665164c696018d01e4c0f1e455c54f7.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c100de9e56dbc8d1531",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "7c100de9e56dbc8d1531",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "31812119277f49a3bd393a64b60df7e3",
+    "url": "/index.html"
+  }
+];
