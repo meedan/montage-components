014bde6ef323fed6d402cce705ec7be7c91a514f
Laurian Gridinoc <laurian@gridinoc.name>
2019-02-02 01:00:39 -0800

    hmmm

--- packages/video-timeline/src/index.js
+++ packages/video-timeline/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
+import Sample from './Sample';
+
 import * as serviceWorker from './serviceWorker';
 
 const root = document.getElementById('react-root');
