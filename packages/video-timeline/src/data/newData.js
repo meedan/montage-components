0e2f69c6eefbbcd806f7e08a123505fbf6c32c9a
Piotr F <hello@piotrf.pl>
2019-03-21 01:09:09 +0700

    Bring in some dummy data.

---
+++ packages/video-timeline/src/data/newData.js
@@ -0,0 +1,12 @@
+const newData = {
+  keep: {
+    services:
+     [
+      { name: "Archive.org", isActive: true, fileUrl: "https://archive.org/x/keep/y/z.mp4" },
+      { name: "Archive.is", isActive: false, fileUrl: null },
+      { name: "Internet Archive", isActive: false, fileUrl: "https://archive.org/x/keep/y/z.mp4" },
+    ]
+  }
+};
+
+export default newData;
