29be569fb3a877b012f9c1ab8737430727061605
Piotr F <hello@piotrf.pl>
2019-03-20 21:24:17 +0700

    Document first custom icon.

---
+++ packages/montage-ui/src/components/icons/KeepIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+const KeepIcon = props => {
+  return (
+    <SvgIcon {...props}>
+      <path d="M8.5455 14.014h-.056v7.8881H2V2.0979h6.4615v7.3007h.056l5.2867-7.3007h7.8322l-7.4406 9.063L22 21.902h-8.1678z" />
+    </SvgIcon>
+  );
+};
+
+export default KeepIcon;
