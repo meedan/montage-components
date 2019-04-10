a11125567115467ede051a7e4c6b247cce463814
Piotr F <hello@piotrf.pl>
2019-04-10 20:16:06 +0200

    Bring in Comment edit mode.

--- packages/video-timeline/src/components/ofTimeline/ofComments/Comment.js
+++ packages/video-timeline/src/components/ofTimeline/ofComments/Comment.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
@@ -13,17 +18,31 @@ import EditIcon from '@material-ui/icons/Edit';
 
 const styles = {
   avatar: {
-    height: 32,
-    width: 32,
-    border: '1px solid white',
+    height: 28,
+    width: 28,
+  },
+  ListItem: {
+    width: '220px',
+  },
+  listItemSecondaryAction: {
+    top: 8,
+    transform: 'none',
   },
 };
 
 function Comment(props) {
   const { isActionable, classes, id, fname, lname, avatar, date, text } = props;
 
+  const [editMode, setEditMode] = useState(false);
+  const [comment, setComment] = useState(text);
+
+  const abbandonCommentEdit = () => {
+    setComment(text);
+    setEditMode(false);
+  };
+
   return (
-    <ListItem key={id} alignItems="flex-start">
+    <ListItem alignItems="flex-start" key={id} className={classes.ListItem}>
       <ListItemAvatar>
         <Avatar
           alt={`${fname} ${lname}`}
@@ -36,14 +55,67 @@ function Comment(props) {
         <Typography variant="caption" color="textSecondary">
           {date}
         </Typography>
-        <Typography variant="body2" color="textSecondary">
-          {text}
-        </Typography>
+        {editMode ? (
+          <Grid container direction="column" spacing={8} wrap="nowrap">
+            <Grid item>
+              <TextField
+                autoFocus
+                defaultValue={comment}
+                fullWidth
+                id="comment"
+                inputProps={{
+                  autoComplete: 'off',
+                  style: { fontSize: '13px' },
+                }}
+                placeholder="Enter comment"
+                required
+                type="text"
+                onChange={e => setComment(e.currentTarget.value)}
+                className={classes.commentInput}
+              />
+            </Grid>
+            <Grid item>
+              <Grid
+                container
+                direction="row-reverse"
+                justify="space-between"
+                wrap="nowrap"
+              >
+                <Grid item>
+                  <Button
+                    color="primary"
+                    disabled={comment.length === 0}
+                    mini
+                    // onClick={createCollection}
+                    size="small"
+                  >
+                    Save
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button mini onClick={abbandonCommentEdit} size="small">
+                    Cancel
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        ) : (
+          <Typography variant="body2" color="textSecondary">
+            {text}
+          </Typography>
+        )}
       </ListItemText>
-      <ListItemSecondaryAction>
+      <ListItemSecondaryAction className={classes.listItemSecondaryAction}>
         {isActionable ? (
-          <IconButton aria-label="Edit">
-            <EditIcon />
+          <IconButton
+            aria-label="Edit"
+            className={classes.editToggle}
+            onClick={() => setEditMode(true)}
+          >
+            <Tooltip title="Edit">
+              <EditIcon fontSize="small" />
+            </Tooltip>
           </IconButton>
         ) : null}
       </ListItemSecondaryAction>
