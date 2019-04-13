5b03f2ea146fd800383ccb18ac970ed2e6f8b919
Piotr F <hello@piotrf.pl>
2019-04-13 15:46:46 +0200

    Cleanup.

--- packages/video-timeline/src/components/ofTimeline/ofComments/Comment.js
+++ packages/video-timeline/src/components/ofTimeline/ofComments/Comment.js
@@ -8,6 +8,7 @@ import Popover from 'material-ui-popup-state/HoverPopover';
 
 import { withStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
+import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
@@ -15,7 +16,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import DeleteIcon from '@material-ui/icons/Delete';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Typography from '@material-ui/core/Typography';
 
