77ac05d4e2a1fe5471ba572b84d1ce14baa228ee
Piotr F <hello@piotrf.pl>
2019-04-19 17:16:31 +0200

    Stash progress on instance popover.

--- packages/montage-ui/src/components/icons/InstanceExpandIcon.js
+++ packages/montage-ui/src/components/icons/InstanceExpandIcon.js
@@ -4,7 +4,7 @@ import SvgIcon from "@material-ui/core/SvgIcon";
 const InstanceExpand = props => {
   return (
     <SvgIcon {...props}>
-      <path d="M12.333 11.333h2v-2L17 12l-2.667 2.667v-2H9.667v2L7 12l2.667-2.667v2h2.666zm-8 6h1.334V6.667H4.333v10.666zm14 0h1.334V6.667h-1.334v10.666z" />
+      <path d="M12.478 11.043h2.87v-2.87L19.174 12l-3.826 3.826v-2.87H8.652v2.87L4.826 12l3.826-3.826v2.87h3.826zM1 19.653h1.913V4.347H1v15.304zm20.087 0H23V4.347h-1.913v15.304z" />
     </SvgIcon>
   );
 };
