1b1b4d3aa8ef1af1960e00c1743ebbb532151811
Piotr F <hello@piotrf.pl>
2019-04-19 17:09:40 +0200

    Document InstanceExpand and Clips icons.

---
+++ packages/montage-ui/src/components/icons/InstanceExpandIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+const InstanceExpand = props => {
+  return (
+    <SvgIcon {...props}>
+      <path d="M12.333 11.333h2v-2L17 12l-2.667 2.667v-2H9.667v2L7 12l2.667-2.667v2h2.666zm-8 6h1.334V6.667H4.333v10.666zm14 0h1.334V6.667h-1.334v10.666z" />
+    </SvgIcon>
+  );
+};
+
+export default InstanceExpand;
