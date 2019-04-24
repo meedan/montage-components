c278e65fc615d5daeae37b7a157ac981f3386669
Piotr F <hello@piotrf.pl>
2019-04-24 14:44:38 +0200

    Bring back handle tooltips.

--- packages/video-timeline/src/components/ofTimeline/Clips.js
+++ packages/video-timeline/src/components/ofTimeline/Clips.js
@@ -180,9 +180,11 @@ class TimelineClips extends Component {
   handle = props => {
     // console.log(props);
     const { value, index, ...restProps } = props;
-    // <Tooltip key={index} placement="top" title={formatTime(value)}>
-    // </Tooltip>
-    return <Handle value={value} {...restProps} />;
+    return (
+      <Tooltip key={index} placement="top" title={formatTime(value)}>
+        <Handle value={value} {...restProps} />
+      </Tooltip>
+    );
   };
 
   onAfterChange = (v, id) => {
