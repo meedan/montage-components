6346d5cf6dc0c61792de7cd39088e1f1c1d10bd5
Laurian Gridinoc <laurian@gridinoc.name>
2019-04-24 19:18:57 +0300

    restoring all the mock data

--- packages/video-timeline/src/data/moreData.js
+++ packages/video-timeline/src/data/moreData.js
@@ -10,27 +10,27 @@ const moreData = {
   project: {
     ...baseData.project,
     collections: [
-      // {
-      //   created: '2019-02-27T14:09:55+00:00',
-      //   id: 81,
-      //   modified: '2019-02-27T14:09:55+00:00',
-      //   name: 'A collection',
-      //   project_id: 1254,
-      // },
-      // {
-      //   created: '2019-02-27T14:10:57+00:00',
-      //   id: 82,
-      //   modified: '2019-02-27T14:10:57+00:00',
-      //   name: 'Another one',
-      //   project_id: 1254,
-      // },
-      // {
-      //   created: '2019-02-27T14:15:05+00:00',
-      //   id: 83,
-      //   modified: '2019-02-27T14:15:05+00:00',
-      //   name: 'Hello',
-      //   project_id: 1254,
-      // },
+      {
+        created: '2019-02-27T14:09:55+00:00',
+        id: 81,
+        modified: '2019-02-27T14:09:55+00:00',
+        name: 'A collection',
+        project_id: 1161,
+      },
+      {
+        created: '2019-02-27T14:10:57+00:00',
+        id: 82,
+        modified: '2019-02-27T14:10:57+00:00',
+        name: 'Another one',
+        project_id: 1161,
+      },
+      {
+        created: '2019-02-27T14:15:05+00:00',
+        id: 83,
+        modified: '2019-02-27T14:15:05+00:00',
+        name: 'Hello',
+        project_id: 1161,
+      },
     ],
   },
 };
