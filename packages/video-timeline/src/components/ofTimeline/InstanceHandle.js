9e70b1216935af59c33553621b6ba8d3689fd7a5
Piotr F <hello@piotrf.pl>
2019-04-29 19:55:36 +0200

    Fix #60: disable click to resize timeline instance.

--- packages/video-timeline/src/components/ofTimeline/InstanceHandle.js
+++ packages/video-timeline/src/components/ofTimeline/InstanceHandle.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
 import { Handle } from 'rc-slider';
-import PopupState, { bindHover, bindPopover } from 'material-ui-popup-state';
+import React, { Component } from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
@@ -33,42 +32,37 @@ class InstanceHandle extends Component {
   render() {
     const { classes, value } = this.props;
     return (
-      <PopupState variant="popover" popupId="instanceHandlePopover">
-        {popupState => (
-          <>
-            <Tooltip placement="top" title={formatTime(value)}>
-              <Handle {...this.props} {...bindHover(popupState)} />
+      <>
+        <Tooltip placement="top" title={formatTime(value)}>
+          <Handle {...this.props} />
+        </Tooltip>
+        <Popover
+          number={20}
+          disableEnforceFocus={false}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          disableRestoreFocus
+        >
+          <div className={classes.Toolbar}>
+            <Tooltip placement="bottom" title="Move backward">
+              <IconButton onClick={e => this.moveBackward(e)}>
+                <ArrowBackIcon fontSize="small" />
+              </IconButton>
             </Tooltip>
-            <Popover
-              {...bindPopover(popupState)}
-              number={20}
-              disableEnforceFocus={false}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
-              }}
-              disableRestoreFocus
-            >
-              <div className={classes.Toolbar}>
-                <Tooltip placement="bottom" title="Move backward">
-                  <IconButton onClick={e => this.moveBackward(e)}>
-                    <ArrowBackIcon fontSize="small" />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip placement="bottom" title="Move forward">
-                  <IconButton onClick={e => this.moveForward(e)}>
-                    <ArrowForwardIcon fontSize="small" />
-                  </IconButton>
-                </Tooltip>
-              </div>
-            </Popover>
-          </>
-        )}
-      </PopupState>
+            <Tooltip placement="bottom" title="Move forward">
+              <IconButton onClick={e => this.moveForward(e)}>
+                <ArrowForwardIcon fontSize="small" />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </Popover>
+      </>
     );
   }
 }
