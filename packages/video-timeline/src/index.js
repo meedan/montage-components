7927a26dc82ffdd6a5851793de3776e790b5eb13
Laurian Gridinoc <laurian@gridinoc.name>
2019-04-29 18:12:24 +0300

    redux + empty store

--- packages/video-timeline/src/index.js
+++ packages/video-timeline/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { configureAppStore } from './configureStore';
+
 import App from './App';
 
+
+const store = configureAppStore();
+
 const root = document.getElementById('react-root');
-if (root) ReactDOM.render(<App />, root);
+if (root) ReactDOM.render(<Provider store={store}><App /></Provider>, root);
