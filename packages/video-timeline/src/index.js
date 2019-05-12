dbbfe8ac23b53067c60ab444126df1cfc2c53753
Laurian Gridinoc <laurian@gridinoc.name>
2019-05-12 21:24:09 +0300

    redux-localstorage-simple

--- packages/video-timeline/src/index.js
+++ packages/video-timeline/src/index.js
@@ -6,7 +6,7 @@ import { configureAppStore } from './configureStore';
 
 import App from './App';
 
-const store = configureAppStore({});
+const store = configureAppStore();
 
 const root = document.getElementById('react-root');
 if (root)
