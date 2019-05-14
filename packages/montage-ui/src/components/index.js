e149259486e56329cf14050fac30cb6a9eda35a4
Piotr F <hello@piotrf.pl>
2019-05-14 18:25:38 +0200

    Rename TempIcons to Icons.

--- packages/montage-ui/src/components/index.js
+++ packages/montage-ui/src/components/index.js
@@ -1,7 +1,7 @@
 export { default as VideoMeta } from "./VideoMeta/VideoMeta";
 
-export { default as CheckIcon } from "./TempIcons/Check";
-export { default as ClipboardIcon } from "./TempIcons/Clipboard";
-export { default as CutIcon } from "./TempIcons/Cut";
-export { default as ExpandIcon } from "./TempIcons/Expand";
-export { default as KeepIcon } from "./TempIcons/Keep";
+export { default as CheckIcon } from "./Icons/Check";
+export { default as ClipboardIcon } from "./Icons/Clipboard";
+export { default as CutIcon } from "./Icons/Cut";
+export { default as ExpandIcon } from "./Icons/Expand";
+export { default as KeepIcon } from "./Icons/Keep";
