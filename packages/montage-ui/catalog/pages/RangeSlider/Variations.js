d387c49711d68fdfdb60550e8d91d14f595ea2af
Piotr F <hello@piotrf.pl>
2019-05-29 19:54:47 +0200

    Cleanup docs.

--- packages/montage-ui/catalog/pages/RangeSlider/Variations.js
+++ packages/montage-ui/catalog/pages/RangeSlider/Variations.js
@@ -9,7 +9,7 @@ export default () => markdown`
 
 # Variations
 
-## Default
+## Clips range slider
 
 ${(
   <ReactSpecimen>
@@ -71,65 +71,7 @@ ${(
   </ReactSpecimen>
 )}
 
-${(
-  <ReactSpecimen>
-    <RangeSlider
-      duration={2645}
-      deleteInstance={() => console.log("deleteInstance")}
-      extendInstance={() => console.log("extendInstance")}
-      instancePopoverChildren={(
-        <>
-          <Tooltip title="Copy to Clips">
-            <IconButton>
-              <CutIcon fontSize="small" />
-            </IconButton>
-          </Tooltip>
-        </>
-)}
-      instances={[
-        {
-          // created: "2019-04-24T10:11:24+00:00",
-          // global_tag_id: 3972,
-          // modified: "2019-04-24T10:13:44+00:00",
-          // project_id: 1254,
-          // project_tag_id: 3994,
-          // video_id: 11503,
-          // video_tag_id: 20674,
-          // youtube_id: "9G8dmsnFjL4"
-          end_seconds: 1963,
-          id: 24100,
-          start_seconds: 1043.23066109155
-        },
-        {
-          // created: "2019-04-24T10:14:05+00:00",
-          // global_tag_id: 3972,
-          // modified: "2019-04-24T10:14:05+00:00",
-          // project_id: 1254,
-          // project_tag_id: 3994,
-          // video_id: 11503,
-          // video_tag_id: 20674,
-          // youtube_id: "9G8dmsnFjL4"
-          end_seconds: 45,
-          id: 24101,
-          start_seconds: 0
-        },
-        {
-          // created: "2019-04-24T10:14:05+00:00",
-          // global_tag_id: 3972,
-          // modified: "2019-04-24T10:14:05+00:00",
-          // project_id: 1254,
-          // project_tag_id: 3994,
-          // video_id: 11503,
-          // video_tag_id: 20674,
-          // youtube_id: "9G8dmsnFjL4"
-          end_seconds: 126,
-          id: 24105,
-          start_seconds: 120
-        }
-      ]}
-    />
-  </ReactSpecimen>
-)}
+## Tags/Places range slider
 
 ${(
   <ReactSpecimen>
@@ -190,6 +132,5 @@ ${(
     />
   </ReactSpecimen>
 )}
-
 
 `;
