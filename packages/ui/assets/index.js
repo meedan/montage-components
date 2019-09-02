22688941d2d106c9e1950e20246dffb0f31b8df6
Piotr F <hello@piotrf.pl>
2019-09-02 15:20:39 +0200

    Add proper comment marker.

--- packages/ui/assets/index.js
+++ packages/ui/assets/index.js
@@ -1,3 +1,4 @@
+export { default as Iconfont } from './fonts/iconfont/style.css';
 export { default as Noto } from './fonts/noto/stylesheet.css';
 export { default as Roboto } from './fonts/roboto/stylesheet.css';
 export { default as favicon } from './images/favicon.ico';
--- packages/video-timeline/src/components/ofTranscript/TranscriptWrapper.js
+++ packages/video-timeline/src/components/ofTranscript/TranscriptWrapper.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components';
 
+import { color } from '@montage/ui';
+
 const TranscriptWrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
   max-width: ${({ stretch }) => (stretch ? '1400px' : '1050px')};
+
+  span[class*='C-']:before {
+    position: absolute;
+    top: -1em;
+    left: -1.1em;
+    color: ${color.brand};
+
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    content: '';
+    font-family: 'iconfont' !important;
+    font-size: 16px;
+    speak: none;
+  }
 `;
 
 export default TranscriptWrapper;
