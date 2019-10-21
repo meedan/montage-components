a3a8c66462a61479a606ddd61eec201179cdf8e8
Piotr F <hello@piotrf.pl>
2019-10-21 16:28:57 +0200

    Clean up.

--- packages/video-timeline/src/components/ofTranscript/HoverPopover.js
+++ packages/video-timeline/src/components/ofTranscript/HoverPopover.js
@@ -33,12 +33,12 @@ class HoverPopover extends Component {
   render() {
     const { classes, tagInstances, placeInstances } = this.props;
 
-    console.group('HoverPopover.js');
-    console.log({ tagInstances });
-    console.log({ placeInstances });
+    // console.group('HoverPopover.js');
+    // console.log({ tagInstances });
+    // console.log({ placeInstances });
     // console.log('props:', this.props);
     // console.log('state:', this.state);
-    console.groupEnd();
+    // console.groupEnd();
 
     return (
       <Popper
